637d9fb4faeb508c056bba81ad5ed69124bea847
MaheshVegada <56226368+MaheshVegada@users.noreply.github.com>
2021-05-17 11:58:33 +0530

    Add files via upload

---
+++ Iterator/NameRepository.java
@@ -0,0 +1,32 @@
+package Iterator;
+
+public class NameRepository implements Container {
+
+    public String names[] = {"Mahesh", "Shyam", "Hemal"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            if (index < names.length) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
--- Iterator/Test.java
+++ Iterator/Test.java
@@ -1,1 +1,13 @@
+package Iterator;
 
+public class Test {
+
+    public static void main(String[] args) {
+        NameRepository namesRepository = new NameRepository();
+        for (Iterator itrVar = namesRepository.getIterator();itrVar.hasNext();) 
+        {
+            String name = (String) itrVar.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
