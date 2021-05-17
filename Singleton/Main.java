01d608ad76edd76cc60d26d688513f062b293d25
MaheshVegada <56226368+MaheshVegada@users.noreply.github.com>
2021-05-17 11:21:47 +0530

    Delete Singleton directory

--- Singleton/Main.java
+++
@@ -1,9 +0,0 @@
-package singletonpattern;
-
-public class Main {
-
-    public static void main(String[] args) {
-        AppConfig obj1 = AppConfig.getInstance();
-        AppConfig obj2 = AppConfig.getInstance();
-    }
-}
